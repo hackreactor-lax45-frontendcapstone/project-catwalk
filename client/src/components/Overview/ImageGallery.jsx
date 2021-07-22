f6e83ee9ef31767878ec3f321fd76b771e2a4e3d
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-22 09:22:44 -0700

    Implement basic div hierarchy and container properties in css

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -2,8 +2,33 @@ import React from 'react';
 
 export default props => {
   return (
-    <div id="body-overview-imagegallery-collapse">
-      ImageGallery
+    <div id="body-overview-imagegallery-default">
+      {/* main image div */}
+      <div id="imagegallery-default-main">
+        <div id="imagegallery-default-main-buttonleft">
+          <button className="imagegallery-button"></button>
+        </div>
+        <div id="imagegallery-default-main-image">
+          <img className="imagegallery-main"></img>
+        </div>
+        <div id="imagegallery-default-main-buttonright">
+          <button className="imagegallery-button"></button>
+        </div>
+      </div>
+      {/* thumbnails div */}
+      <div id="imagegallery-default-thumbnails">
+        <div id="imagegallery-default-thumbnails-buttonleft">
+          <button className="imagegallery-button"></button>
+        </div>
+        <div id="imagegallery-default-thumbnails-image">
+          <img className="imagegallery-thumbnail"></img>
+          <img className="imagegallery-thumbnail"></img>
+          <img className="imagegallery-thumbnail"></img>
+        </div>
+        <div id="imagegallery-default-thumbnails-buttonright">
+          <button className="imagegallery-button"></button>
+        </div>
+      </div>
     </div>
   );
 };
