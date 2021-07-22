eb6c45537ea98266a85fb582a9c49ca3438f514b
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-22 09:38:24 -0700

    Refactor ImageGallery to be a stateful component as to track selected image index when styleselector changes style

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -1,34 +1,44 @@
 import React from 'react';
 
-export default props => {
-  return (
-    <div id="body-overview-imagegallery-default">
-      {/* main image div */}
-      <div id="imagegallery-default-main">
-        <div id="imagegallery-default-main-buttonleft">
-          <button className="imagegallery-button"></button>
+class ImageGallery extends React.Component {
+  constructor() {
+    super();
+
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <div id="body-overview-imagegallery-default">
+        {/* main image div */}
+        <div id="imagegallery-default-main">
+          <div id="imagegallery-default-main-buttonleft">
+            <button className="imagegallery-button"></button>
+          </div>
+          <div id="imagegallery-default-main-image">
+            <img className="imagegallery-main"></img>
+          </div>
+          <div id="imagegallery-default-main-buttonright">
+            <button className="imagegallery-button"></button>
+          </div>
         </div>
-        <div id="imagegallery-default-main-image">
-          <img className="imagegallery-main"></img>
-        </div>
-        <div id="imagegallery-default-main-buttonright">
-          <button className="imagegallery-button"></button>
+        {/* thumbnails div */}
+        <div id="imagegallery-default-thumbnails">
+          <div id="imagegallery-default-thumbnails-buttonleft">
+            <button className="imagegallery-button"></button>
+          </div>
+          <div id="imagegallery-default-thumbnails-image">
+            <img className="imagegallery-thumbnail"></img>
+            <img className="imagegallery-thumbnail"></img>
+            <img className="imagegallery-thumbnail"></img>
+          </div>
+          <div id="imagegallery-default-thumbnails-buttonright">
+            <button className="imagegallery-button"></button>
+          </div>
         </div>
       </div>
-      {/* thumbnails div */}
-      <div id="imagegallery-default-thumbnails">
-        <div id="imagegallery-default-thumbnails-buttonleft">
-          <button className="imagegallery-button"></button>
-        </div>
-        <div id="imagegallery-default-thumbnails-image">
-          <img className="imagegallery-thumbnail"></img>
-          <img className="imagegallery-thumbnail"></img>
-          <img className="imagegallery-thumbnail"></img>
-        </div>
-        <div id="imagegallery-default-thumbnails-buttonright">
-          <button className="imagegallery-button"></button>
-        </div>
-      </div>
-    </div>
-  );
-};
+    );
+  }
+}
+
+export default ImageGallery;
