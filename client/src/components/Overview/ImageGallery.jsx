a7f2c9b8efc9ebc8cda5eb91b9bb57d80a5afe4b
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-22 16:10:47 -0700

    Implement thumbnail div and selected image div

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -1,11 +1,43 @@
 import React from 'react';
 import '../../../dist/styles/overview/ImageGallery.css';
+import data from '../../data/product_styles.js';
+import _ from 'lodash';
 
 class ImageGallery extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    var styles = _.reduce(data.results, (styleMemo, styleObject) => {
+      var stylePhotos = _.reduce(styleObject.photos, (photoMemo, photoObject) => {
+        photoMemo.thumbnail.push(photoObject.thumbnail_url);
+        photoMemo.url.push(photoObject.url);
+        return photoMemo;
+      }, {thumbnail: [], url: []});
+
+      styleMemo.push(stylePhotos);
+      return styleMemo;
+    }, []);
+
+    this.state = {
+      styles: styles, // all styles
+      styleIndex: 0, // index of selected style
+      style: styles[0], // selected style
+      thumbnailIndex: 0,
+    };
+
+    this.selectImage = this.selectImage.bind(this);
+  }
+
+  setStyle(styleIndex) {
+    this.setState({
+      styleIndex
+    });
+  }
+
+  selectImage(thumbnailIndex) {
+    this.setState({
+      thumbnailIndex
+    });
   }
 
   render() {
@@ -17,7 +49,10 @@ class ImageGallery extends React.Component {
             <button className="imagegallery-button"></button>
           </div>
           <div id="imagegallery-default-main-image">
-            <img className="imagegallery-main"></img>
+            <img
+              className="imagegallery-main"
+              src={this.state.style.url[this.state.thumbnailIndex]}
+            ></img>
           </div>
           <div id="imagegallery-default-main-button">
             <button className="imagegallery-button"></button>
@@ -29,9 +64,15 @@ class ImageGallery extends React.Component {
             <button className="imagegallery-button"></button>
           </div>
           <div id="imagegallery-default-thumbnails-image">
-            <img className="imagegallery-thumbnail"></img>
-            <img className="imagegallery-thumbnail"></img>
-            <img className="imagegallery-thumbnail"></img>
+            {_.map(this.state.style.thumbnail, (thumbnail, i) => {
+              return <img
+                key={i}
+                className="imagegallery-thumbnail"
+                src={thumbnail}
+                onClick={() => this.selectImage(i)}
+              ></img>;
+            })}
+
           </div>
           <div id="imagegallery-default-thumbnails-button">
             <button className="imagegallery-button"></button>
