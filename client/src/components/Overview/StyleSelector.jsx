0024e10425f93a22746468d6ed45e0d51410ba0a
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-22 11:23:48 -0700

    Boiler Plate for StyleSelector

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -3,7 +3,21 @@ import React from 'react';
 export default props => {
   return (
     <div id="body-overview-styleselector">
-      StyleSelector
+      <div className="selection-title">Selected Style Title</div>
+      <div className="style-thumbnails">THUMBNAILS</div>
+      <div className="btn-more-styles">
+        <button>More Styles</button>
+      </div>
     </div>
   );
 };
+
+/*
+
+<div OUTER_DIV>
+  <div TITLE OF SELECTION></div>
+  <div STYLE THUMBNAILS></div>
+  <div MORE STYLES BUTTON></div>
+</div>
+
+*/
