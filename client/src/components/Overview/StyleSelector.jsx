8602a54373917908b9127fa38a7d629cb696ecda
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-22 11:51:15 -0700

    Class refactor

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -1,16 +1,25 @@
 import React from 'react';
 
-export default props => {
-  return (
-    <div id="body-overview-styleselector">
-      <div className="selection-title">Selected Style Title</div>
-      <div className="style-thumbnails">THUMBNAILS</div>
-      <div className="btn-more-styles">
-        <button>More Styles</button>
+
+class StyleSelector extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return (
+      <div id="body-overview-styleselector">
+        <div className="selection-title">Selected Style Title</div>
+        <div className="style-thumbnails">THUMBNAILS</div>
+        <div className="btn-more-styles">
+          <button>More Styles</button>
       </div>
     </div>
-  );
-};
+    )
+  }
+}
+
+export default StyleSelector;
 
 /*
 
