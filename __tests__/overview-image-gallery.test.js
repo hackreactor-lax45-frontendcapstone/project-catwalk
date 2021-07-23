07267f9378fb41d8fb0458070c77d8b4c82cb9b2
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-23 15:41:19 -0700

    Rename test files to follow desired syntax, fill outtest files with at least one test

--- __tests__/overview-image-gallery.test.js
+++ __tests__/overview-image-gallery.test.js
@@ -4,3 +4,6 @@ Import any required packages:
 const package = require('package_url');
  */
 
+it('Confirms that Jest works', () => {
+  expect(2).toEqual(2);
+});
---
+++ __tests__/overview-style-selector.test.js
@@ -0,0 +1,9 @@
+/*
+Import any required packages:
+
+const package = require('package_url');
+ */
+
+it('Confirms that Jest works', () => {
+  expect(1).toEqual(1);
+});
