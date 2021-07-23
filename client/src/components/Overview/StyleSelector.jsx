4876db1710a90dac412ef0ce73266d5323186b20
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-23 08:38:25 -0700

    StyleSelector rendered with sample data, some functionality implemented

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import axios from 'axios';
 
+import StylesList from './StylesList.jsx'
 import atelierAPI from '../../lib/atelierAPI.js'
 import '../../../dist/styles/overview/StyleSelector.css'
 
@@ -9,44 +10,62 @@ class StyleSelector extends React.Component {
     super(props)
 
     this.state = {
-      product: [],
-      thumbnails: [],
-      selected: ''
+      product: []
     }
+
+    this.handleSelected = this.handleSelected.bind(this);
   }
 
   componentDidMount() {
     axios.get(`${atelierAPI.url}/products/16060/styles`, {
       headers: atelierAPI.headers
     })
-      .then(res => {
-        let thumbnails = res.data.results.map(style => {
-          return style.photos;
-        });
-        thumbnails = [].concat(...thumbnails);
+      .then(styles => {
+        // let thumbnails = res.data.results.map(style => {
+        //   return style.photos;
+        // });
+        // thumbnails = [].concat(...thumbnails);
+
+        let selected;
+        styles.data.results.forEach(style => {
+          if (style['default?'] === true) {
+            selected = style.name;
+          }
+        })
+
         this.setState({
-          product: res.data.results,
-          thumbnails,
-          selected: res.data.results[0].style_id
+          product: styles.data.results,
+          selected
+          // thumbnails
         })
       })
       .catch(err => console.error(err));
   }
 
+  handleSelected(e) {
+    e.preventDefault;
+    console.log(e.target);
+    this.setState({
+      selected: e.target.name
+    });
+  }
+
   render() {
     return (
       <div id="body-overview-styleselector">
-        <div className="selection-title">Selected Style Title</div>
+        <div className="selection-title">{this.state.selected}</div>
         <div className="style-thumbnails">
           {/* <button className="btn-more-styles">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
             <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"/>
             </svg>
           </button> */}
-          {this.state.thumbnails.map(thumbnail => {
-            return <span key={thumbnail.url} className="thumbnail-container">
-              <img className="thumbnail" src={thumbnail.thumbnail_url}></img>
-            </span>
+          {this.state.product.map(style => {
+            return <StylesList
+              key={style.style_id}
+              style={style}
+              selected={this.state.selected}
+              handleSelected={this.handleSelected}/>
           })}
           {/* <button className="btn-more-styles">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-compact-down" viewBox="0 0 16 16">
@@ -60,13 +79,3 @@ class StyleSelector extends React.Component {
 }
 
 export default StyleSelector;
-
-/*
-
-<div OUTER_DIV>
-  <div TITLE OF SELECTION></div>
-  <div STYLE THUMBNAILS></div>
-  <div MORE STYLES BUTTON></div>
-</div>
-
-*/
