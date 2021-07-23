6b604757fc0703c7df5be16ce8aa9bd588c77b93
Anna <a.solilee@gmail.com>
2021-07-23 10:27:27 -0700

    Made changes to config/example.config.js

--- client/src/config/example.config.js
+++ client/src/config/example.config.js
@@ -2,5 +2,5 @@
 Create a file named `config.js` in this same folder with your real github token.
  */
 
-const TOKEN = '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';
+const TOKEN = 'insert token';
 export default TOKEN;
