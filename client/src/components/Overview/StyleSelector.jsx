08614bf2d6ad0a060094dd29502229fee6d9a464
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-23 00:36:15 -0700

    Implementing selected functionality

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -10,7 +10,8 @@ class StyleSelector extends React.Component {
 
     this.state = {
       product: [],
-      thumbnails: []
+      thumbnails: [],
+      selected: ''
     }
   }
 
@@ -20,12 +21,13 @@ class StyleSelector extends React.Component {
     })
       .then(res => {
         let thumbnails = res.data.results.map(style => {
-          return style.photos
+          return style.photos;
         });
         thumbnails = [].concat(...thumbnails);
         this.setState({
           product: res.data.results,
-          thumbnails
+          thumbnails,
+          selected: res.data.results[0].style_id
         })
       })
       .catch(err => console.error(err));
@@ -36,19 +38,22 @@ class StyleSelector extends React.Component {
       <div id="body-overview-styleselector">
         <div className="selection-title">Selected Style Title</div>
         <div className="style-thumbnails">
+          {/* <button className="btn-more-styles">
+            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"/>
+            </svg>
+          </button> */}
           {this.state.thumbnails.map(thumbnail => {
-            return <span className="thumbnail-container">
+            return <span key={thumbnail.url} className="thumbnail-container">
               <img className="thumbnail" src={thumbnail.thumbnail_url}></img>
             </span>
           })}
-        </div>
-        <div className="btn-more-styles">
-          <button>
+          {/* <button className="btn-more-styles">
             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-compact-down" viewBox="0 0 16 16">
             <path fillRule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
             </svg>
-          </button>
-      </div>
+          </button> */}
+        </div>
     </div>
     )
   }
