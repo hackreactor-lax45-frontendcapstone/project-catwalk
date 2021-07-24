43e439e7e5715c44bc5e2e4fb1c1bebd22abda55
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-24 12:13:39 -0700

    Revert contents of ImageGallery.jsx manually

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -1,12 +1,6 @@
 import React from 'react';
 
-import { useSelector, useDispatch } from 'react-redux';
-
 export default props => {
-
-  var state = useSelector(state => state);
-  console.log(state);
-
   return (
     <div id="body-overview-imagegallery-collapse">
       ImageGallery
