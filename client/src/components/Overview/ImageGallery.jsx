8559c100377ffba5014e6351442fe0c52d991059
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-24 16:33:34 -0700

    Refactor ImageGallery.jsx for compatibility with Redux

--- client/src/components/Overview/ImageGallery.jsx
+++ client/src/components/Overview/ImageGallery.jsx
@@ -1,103 +1,73 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+
 import '../../../dist/styles/overview/ImageGallery.css';
-import data from '../../data/product_styles.js';
+import actions from '../../state/actions/index.js';
 import _ from 'lodash';
 
-class ImageGallery extends React.Component {
-  constructor(props) {
-    super(props);
+export default () => {
 
-    var styles = _.reduce(data.results, (styleMemo, styleObject) => {
-      var stylePhotos = _.reduce(styleObject.photos, (photoMemo, photoObject) => {
-        photoMemo.thumbnail.push(photoObject.thumbnail_url);
-        photoMemo.url.push(photoObject.url);
-        return photoMemo;
-      }, {thumbnail: [], url: []});
+  const dispatch = useDispatch();
 
-      styleMemo.push(stylePhotos);
-      return styleMemo;
-    }, []);
-
-    this.state = {
-      styles: styles, // all styles
-      styleIndex: 0, // index of selected style
-      style: styles[0], // selected style
-      thumbnailIndex: 0,
+  const state = useSelector(state => {
+    if (_.isNil(state.product.styleInfo.results)) {
+      return undefined;
+    }
+    return {
+      style: state.product.styleInfo.results[state.style],
+      thumbnail: state.thumbnail,
     };
+  });
 
-    this.selectImage = this.selectImage.bind(this);
-    this.incrementImage = this.incrementImage.bind(this);
-  }
-
-  componentDidMount() {
-    // this.thumbnailWidth = document.getElementById('imagegallery-default-thumbnails-image').offsetWidth;
-    // var imageWidth = document.getElementById(`image-gallery-thumbnail-${index}`).offsetWidth;
-    // this.setState({
-    //   scrollPosition: document.getElementById(`image-gallery-thumbnail-${this.state.thumbnailIndex}`).offsetWidth
-    // });
-  }
-
-  setStyle(styleIndex) {
-    this.setState({
-      styleIndex
-    });
-  }
-
-  incrementImage(increment) {
-    var index = this.state.thumbnailIndex + increment;
-    index = index < 0 ? 0 : index;
-    index = index > this.state.style.url.length - 1 ? this.state.style.url.length - 1 : index;
-
-    // document.getElementById('imagegallery-default-thumbnails-image').scrollLeft = scrollPosition;
-
-    this.setState({
-      thumbnailIndex: index,
-    });
-  }
-
-  selectImage(thumbnailIndex) {
-    this.setState({
-      thumbnailIndex
-    });
-  }
-
-  render() {
+  if (state) {
     return (
       <div id="body-overview-imagegallery-default">
+
         {/* main image div */}
         <div id="imagegallery-default-main">
           <div id="imagegallery-default-main-button">
-            <button className="imagegallery-button"></button>
-            <button className="imagegallery-button"></button>
+            <button
+              className="imagegallery-button"
+              onClick={() => {
+                var thumbnail = state.thumbnail - 1;
+                if (thumbnail >= 0) {
+                  dispatch(actions.selectThumbnail(thumbnail));
+                }
+              }}
+            ></button>
+            <button
+              className="imagegallery-button"
+              onClick={() => {
+                var thumbnail = state.thumbnail + 1;
+                var max = state.style.photos.length - 1;
+                if (thumbnail <= max) {
+                  dispatch(actions.selectThumbnail(thumbnail));
+                }
+              }}
+            ></button>
           </div>
           <div id="imagegallery-default-main-image">
             <img
               className="imagegallery-main"
-              src={this.state.style.url[this.state.thumbnailIndex]}
+              src={state.style.photos[state.thumbnail].url}
             ></img>
           </div>
         </div>
+
         {/* thumbnails div */}
         <div id="imagegallery-default-thumbnails">
           <div id="imagegallery-default-thumbnails-button">
-            <button
-              className="imagegallery-button"
-              onClick={(e) => this.incrementImage(-1)}
-            ></button>
-            <button
-              className="imagegallery-button"
-              onClick={(e) => this.incrementImage(1)}
-            ></button>
+            <button className="imagegallery-button"></button>
+            <button className="imagegallery-button"></button>
           </div>
           <div id="imagegallery-default-thumbnails-image">
-            {_.map(this.state.style.thumbnail, (thumbnail, i) => {
-              var imageClass = 'imagegallery-thumbnail' + (this.state.thumbnailIndex === i ? '-selected' : '');
+            {_.map(state.style.photos, (photo, i) => {
               return <img
                 key={i}
                 id={`image-gallery-thumbnail-${i}`}
-                className={imageClass}
-                src={thumbnail}
-                onClick={() => this.selectImage(i)}
+                className={`imagegallery-thumbnail${state.thumbnail === i ? '-selected' : ''}`}
+                src={photo.thumbnail_url}
+                onClick={() => dispatch(actions.selectThumbnail(i))}
               ></img>;
             })}
           </div>
@@ -105,6 +75,6 @@ class ImageGallery extends React.Component {
       </div>
     );
   }
-}
+  return (<div>Loading...</div>);
 
-export default ImageGallery;
+};
--- client/src/state/reducers/index.js
+++ client/src/state/reducers/index.js
@@ -9,13 +9,13 @@ import cart from './addToCart';
 import related from './setRelated';
 
 const allReducers = {
-  product,
-  style,
-  thumbnail,
-  size,
-  quantity,
-  cart,
-  related,
+  product, // object { productID: 16060, productInfo: { /products/:product_id }, styleInfo: { /products/:product_id/styles } }
+  style, // number e.g., 0 corresponding to style index 0
+  thumbnail, // number e.g., 0 corresponding to thumbnail index 0
+  size, // string e.g., 'S', 'M', 'L'
+  quantity, // number
+  cart, // array of objects, each object representing a purchased product/style/size/qty
+  related, // array of product_ids
 };
 
 export default combineReducers(allReducers);
