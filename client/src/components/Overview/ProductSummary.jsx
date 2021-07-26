0599800295b0879494973bf97e62ea6ea4d172e9
Anna <a.solilee@gmail.com>
2021-07-25 20:36:54 -0700

    Added sale price to page and style changes to price feature if product has sale price

--- client/src/components/Overview/ProductSummary.jsx
+++ client/src/components/Overview/ProductSummary.jsx
@@ -16,7 +16,7 @@ const ProductSummary = () => {
   });
 
   console.log(styleInfo);
-  if (styleInfo) {
+  if (styleInfo && !styleInfo.price.sale_price) {
     return (
       <div id="body-overview-productsummary">
         <div id="body-overview-starrating">
@@ -30,8 +30,23 @@ const ProductSummary = () => {
         <div id="body-overview-category">{productInfo.category}</div>
         <div id="body-overview-name">{productInfo.name}</div>
         <div id="body-overview-price">${styleInfo.price.original_price} USD</div>
-
-
+      </div>
+    );
+  } else if (styleInfo && !styleInfo.price.sale_price) {
+    return (
+      <div id="body-overview-productsummary">
+        <div id="body-overview-starrating">
+          <div id="body-overview-star">
+            <div className="stars-outer">
+              <div className="stars-inner"></div>
+            </div>
+          </div>
+          <div id="body-overview-count">Read all insert number reviews</div>
+        </div>
+        <div id="body-overview-category">{productInfo.category}</div>
+        <div id="body-overview-name">{productInfo.name}</div>
+        <div id="body-overview-oldprice">${styleInfo.price.original_price} USD</div>
+        <div id="body-overview-saleprice">${styleInfo.price.original_price - styleInfo.price.sale_price} USD</div>
       </div>
     );
   } else {
