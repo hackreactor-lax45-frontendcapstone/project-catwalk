22f6163cd3bc81b5d4b71a5a4ff16731e7da2610
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-26 01:02:16 -0700

    Cleaned up code, button scroll working, ready for css layout

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -5,7 +5,6 @@ import actions from '../../state/actions/index.js';
 import '../../../dist/styles/overview/StyleSelector.css';
 
 const StyleSelector = () => {
-
   const dispatch = useDispatch();
 
   const styles = useSelector(state => {
@@ -24,16 +23,16 @@ const StyleSelector = () => {
 
   if (styles) {
     const handleClick = (e) => {
+      let thmb = document.getElementsByClassName('style-thumbnails');
+
       if (e.target.id === 'down') {
-        let thmb = document.getElementsByClassName('style-thumbnails');
         thmb[0].scrollBy({
-          top:104,
+          top:100,
           behavior: 'smooth'
         })
       } else if (e.target.id === 'up') {
-        let thmb = document.getElementsByClassName('style-thumbnails');
           thmb[0].scrollBy({
-            top:-104,
+            top:-100,
             behavior: 'smooth'
           })
       }
@@ -43,8 +42,17 @@ const StyleSelector = () => {
       <div id="body-overview-styleselector">
         <div className="selection-title">{selected.name}</div>
         <button onClick={handleClick} className="btn-more-styles">{
-          <svg id="up" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-compact-up" viewBox="0 0 16 16">
-          <path fillRule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"/>
+          <svg
+            id="up"
+            xmlns="http://www.w3.org/2000/svg"
+            width="16" height="16"
+            fill="currentColor"
+            className="bi bi-chevron-compact-up"
+            viewBox="0 0 16 16">
+          <path
+            fillRule="evenodd"
+            d={`M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8
+            6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z`}/>
         </svg>
         }</button>
         <div className="style-thumbnails">
@@ -71,14 +79,23 @@ const StyleSelector = () => {
           })}
         </div>
           <button onClick={handleClick} className="btn-more-styles">{
-            <svg id="down" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-compact-down" viewBox="0 0 16 16">
-            <path fillRule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
+            <svg
+              id="down"
+              xmlns="http://www.w3.org/2000/svg"
+              width="16" height="16"
+              fill="currentColor"
+              className="bi bi-chevron-compact-down"
+              viewBox="0 0 16 16">
+            <path
+              fillRule="evenodd"
+              d={`M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1
+              1.448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z`}/>
           </svg>
           }</button>
     </div>
     )
   }
-  return <div></div>
+  return <div>Loading...</div>
 }
 
 export default StyleSelector;
