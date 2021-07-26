d8e83d9ada360f949014e0f612d92964e8f05af5
Anna <a.solilee@gmail.com>
2021-07-26 16:46:31 -0700

    Made suggested changes

--- __tests__/overview-product-summary.test.js
+++ __tests__/overview-product-summary.test.js
@@ -1,16 +1,22 @@
+import TestRenderer from 'react-test-renderer';
+import ProductDescription from '../client/src/components/Overview/ProductDescription.jsx';
+import ProductSummary from '../client/src/components/Overview/ProductSummary.jsx';
+
 it('Confirms that Jest works', () => {
   expect(3).toEqual(3);
 });
 
-// it('Confirms product name', () => {
+it('Confirms product name', () => {
+  const testRenderer = TestRenderer.create(ProductSummary);
+  const testInstance = testRenderer.root;
 
-// });
+  expect(testInstance.findByProps({id: 'body-overview-name'}).children).toEqual(['Heir Force Ones']);
+});
 
-// {
-//   "id": 16073,
-//   "name": "Hellen Socks",
-//   "slogan": "Facilis quis eius sed fuga similique aliquid provident dolorem provident.",
-//   "description": "Aut voluptatibus nemo culpa atque aspernatur. Similique aut similique fugit ut sunt. Et cum similique optio deserunt a temporibus pariatur aut. Incidunt labore occaecati rerum est quod molestiae dolorem. Quo eos ex architecto.",
-//   "category": "Socks",
-//   "default_price": "924.00"
-// }
+xit('Confirms product slogan', () => {
+  const testRenderer = TestRenderer.create(ProductDescription);
+  const testInstance = testRenderer.root;
+
+  expect(testInstance.findByProps({id: 'body-overview-slogan'}).children).toEqual(['A sneaker dynasty']);
+});
+
--- client/src/components/Overview/ProductSummary.jsx
+++ client/src/components/Overview/ProductSummary.jsx
@@ -4,19 +4,31 @@ import AtelierApi from '../../lib/atelierAPI.js';
 import { useSelector } from 'react-redux';
 
 const ProductSummary = () => {
-  const productInfo = useSelector((state) => state.product.productInfo);
 
-  const styleInfo = useSelector(state => {
+  const state = useSelector(state => {
     if (state.product.styleInfo.results === undefined) {
       return undefined;
     }
     return {
+      productInfo: state.product.productInfo,
       price: state.product.styleInfo.results[state.style]
     };
   });
 
-  console.log(styleInfo);
-  if (styleInfo && !styleInfo.price.sale_price) {
+  const priceHelper = (style) => {
+    if (style.sale_price === null) {
+      return (<div id="body-overview-price">${style.original_price} USD</div>);
+    } else {
+      return (
+        <div>
+          <div id="body-overview-oldprice">${style.original_price} USD</div>
+          <div id="body-overview-saleprice">${style.sale_price} USD</div>
+        </div>
+      );
+    }
+  };
+
+  if (state) {
     return (
       <div id="body-overview-productsummary">
         <div id="body-overview-starrating">
@@ -27,30 +39,14 @@ const ProductSummary = () => {
           </div>
           <div id="body-overview-count">Read all insert number reviews</div>
         </div>
-        <div id="body-overview-category">{productInfo.category}</div>
-        <div id="body-overview-name">{productInfo.name}</div>
-        <div id="body-overview-price">${styleInfo.price.original_price} USD</div>
+        <div id="body-overview-category">{state.productInfo.category}</div>
+        <div id="body-overview-name">{state.productInfo.name}</div>
+        {priceHelper(state.price)}
       </div>
     );
-  } else if (styleInfo && styleInfo.price.sale_price) {
-    return (
-      <div id="body-overview-productsummary">
-        <div id="body-overview-starrating">
-          <div id="body-overview-star">
-            <div className="stars-outer">
-              <div className="stars-inner"></div>
-            </div>
-          </div>
-          <div id="body-overview-count">Read all insert number reviews</div>
-        </div>
-        <div id="body-overview-category">{productInfo.category}</div>
-        <div id="body-overview-name">{productInfo.name}</div>
-        <div id="body-overview-oldprice">${styleInfo.price.original_price} USD</div>
-        <div id="body-overview-saleprice">${styleInfo.price.sale_price} USD</div>
-      </div>
-    );
-  } else {
-    return null;
+
+  } else if (!state) {
+    return (<div>Loading...</div>);
   }
 
 };
