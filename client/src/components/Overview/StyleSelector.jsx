9abdebe184502dfe2f234f7912834f726f4dda5e
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-26 00:49:35 -0700

    Implemented button scroll feature* Ready to work on CSS layout

--- client/src/components/Overview/StyleSelector.jsx
+++ client/src/components/Overview/StyleSelector.jsx
@@ -23,9 +23,30 @@ const StyleSelector = () => {
   });
 
   if (styles) {
+    const handleClick = (e) => {
+      if (e.target.id === 'down') {
+        let thmb = document.getElementsByClassName('style-thumbnails');
+        thmb[0].scrollBy({
+          top:104,
+          behavior: 'smooth'
+        })
+      } else if (e.target.id === 'up') {
+        let thmb = document.getElementsByClassName('style-thumbnails');
+          thmb[0].scrollBy({
+            top:-104,
+            behavior: 'smooth'
+          })
+      }
+    }
+
     return (
       <div id="body-overview-styleselector">
         <div className="selection-title">{selected.name}</div>
+        <button onClick={handleClick} className="btn-more-styles">{
+          <svg id="up" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-compact-up" viewBox="0 0 16 16">
+          <path fillRule="evenodd" d="M7.776 5.553a.5.5 0 0 1 .448 0l6 3a.5.5 0 1 1-.448.894L8 6.56 2.224 9.447a.5.5 0 1 1-.448-.894l6-3z"/>
+        </svg>
+        }</button>
         <div className="style-thumbnails">
           {_.map(styles, (style, i) => {
             return <span key={style.style_id} className="thumbnail-container">
@@ -49,6 +70,11 @@ const StyleSelector = () => {
             </span>
           })}
         </div>
+          <button onClick={handleClick} className="btn-more-styles">{
+            <svg id="down" xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" className="bi bi-chevron-compact-down" viewBox="0 0 16 16">
+            <path fillRule="evenodd" d="M1.553 6.776a.5.5 0 0 1 .67-.223L8 9.44l5.776-2.888a.5.5 0 1 1 .448.894l-6 3a.5.5 0 0 1-.448 0l-6-3a.5.5 0 0 1-.223-.67z"/>
+          </svg>
+          }</button>
     </div>
     )
   }
