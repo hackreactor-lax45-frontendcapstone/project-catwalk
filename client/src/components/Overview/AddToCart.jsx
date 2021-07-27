59e93072b9c8313d3a30aed5553a166ecc778439
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-27 15:48:52 -0700

    Minor layout change, and update useSelected call to return all desired values from global state

--- client/src/components/Overview/AddToCart.jsx
+++ client/src/components/Overview/AddToCart.jsx
@@ -1,58 +1,51 @@
+/* eslint-disable semi */
 import React, {useState} from 'react';
 import SizeRenderEntry from './SizeRenderEntry.jsx';
 import QtyRenderEntry from './qtyRenderEntry.jsx';
 
 import '../../../dist/styles/overview/AddToCart.css';
 
-import { useSelector, useDispatch } from "react-redux";
-import SetSelectSize from "../../state/actions/selectSize.js"
-import SetSelectQty from "../../state/actions/selectQuantity.js"
+import { useSelector, useDispatch } from 'react-redux';
+import SetSelectSize from '../../state/actions/selectSize.js'
+import SetSelectQty from '../../state/actions/selectQuantity.js'
 import setToCart from '../../state/actions/addToCart.js'
 
 const AddToCart = () => {
 
   const dispatch = useDispatch();
 
-
-  const style = useSelector(state => {
+  const state = useSelector(state => {
     if (!state.product.styleInfo.results) {
       return undefined;
     }
-    return state.product.styleInfo.results;
-  });
-  const selected = useSelector(state => {
-    if (!style) {
-      return undefined;
+    return {
+      style: state.product.styleInfo.results,
+      selected: state.product.styleInfo.results[state.style],
+      currentProduct: state.product,
+      currentSize: state.size,
+      currentQty: state.quantity,
     }
-    return style[state.style];
-  });
+  })
 
+  //local state to keep track of selected quantity, stock status, size selection status
+  var [qty, updateQty] = useState(0)
+  var [isOutofStock, updateStockStatus] = useState('false')
+  var [isSizeSelected, updateIsSizeSelected] = useState('false')
 
-  const currentProduct = useSelector(state => {return state.product})
-  const currentSize = useSelector(state => {return state.size})
-  const currentQty = useSelector(state => {return state.quantity})
-
-
-//this handler function should update global state of [size, qty, skus#]
+  //this handler function should update global state of [size, qty, skus#]
   const handleChange = (event) => {
-    if (event.target.value === "outofstock") {
-      updateIsSizeSelected("false")
+    if (event.target.value === 'outofstock') {
+      updateIsSizeSelected('false')
       return null;
 
     } else {
-      updateStockStatus("false")
-      updateIsSizeSelected("true")
+      updateStockStatus('false')
+      updateIsSizeSelected('true')
       var selectedStyle = JSON.parse(event.target.value)
-        dispatch(SetSelectSize(selectedStyle.size))
-        dispatch(SetSelectQty(selectedStyle.quantity))
+      dispatch(SetSelectSize(selectedStyle.size))
+      dispatch(SetSelectQty(selectedStyle.quantity))
     }
   }
-
-  //local state to keep track of selected quantity, stock status, size selection status
-  var [qty,updateQty] = useState(0)
-  var [isOutofStock,updateStockStatus] = useState('false')
-  var [isSizeSelected,updateIsSizeSelected] = useState('false')
-
 
   const handleSelectQty = (event) => {
     updateQty(event.target.value)
@@ -65,7 +58,7 @@ const AddToCart = () => {
   }
 
   const buttonHandler = () => {
-    dispatch(setToCart(currentProduct["productID"], selected["style_id"],currentSize, qty))
+    dispatch(setToCart(currentProduct['productID'], selected['style_id'],currentSize, qty))
   }
 
   const renderCartButton = function() {
@@ -74,17 +67,18 @@ const AddToCart = () => {
       updateIsSizeSelected('true')
       return <div></div>
     } else if (isSizeSelected === 'false') {
-      return <button className="addtocart-button" onClick={buttonHandlerNoSelected} >Add to Cart</button>
+      return <button className='addtocart-button' onClick={buttonHandlerNoSelected} >Add to Cart</button>
     } else {
-      return <button className="addtocart-button" onClick={buttonHandler} >Add to Cart</button>
+      return <button className='addtocart-button' onClick={buttonHandler} >Add to Cart</button>
     }
   }
 
+  if (state) {
+    let { style, selected, currentProduct, currentSize, currentQty } = state;
 
-  if(style && selected) {
     return (
-      <div id="body-overview-addtocart">
-        <div id="dropdowns">
+      <div id='body-overview-addtocart'>
+        <div id='dropdowns'>
           <div>
             <SizeRenderEntry selected={selected} handleChange={handleChange} updateStockStatus={updateStockStatus}/>
           </div>
@@ -101,7 +95,4 @@ const AddToCart = () => {
 
 }
 
-
 export default AddToCart;
-
-
