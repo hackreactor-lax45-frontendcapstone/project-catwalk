d49347f7c5475e17e2ebca2538a6f43359a1b598
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-27 11:23:15 -0700

    Implement scrolling in related items

--- client/src/components/RelatedItems/RelatedProductsList.jsx
+++ client/src/components/RelatedItems/RelatedProductsList.jsx
@@ -7,6 +7,18 @@ import axios from 'axios';
 
 import _ from 'lodash';
 
+const IMAGE_WIDTH = 140;
+
+const scroll = (direction) => {
+  let el = document.getElementById('related-image-container');
+  let pos = el.scrollLeft + direction * IMAGE_WIDTH;
+  el.scrollTo({
+    top: 0,
+    left: pos,
+    behavior: 'smooth'
+  });
+};
+
 export default props => {
 
   const related = useSelector(state => state.related);
@@ -14,18 +26,36 @@ export default props => {
   if (!related.returned) {
     return (<div>Loading...</div>);
   }
-  return _.map(related.styles, (style, i) => {
-
-    return (
-      <div id={`related-product-${i}`} key={i}>
-        <div
-          style={{
-            backgroundImage: `url(${style.results[0].photos[0].thumbnail_url})`,
-            backgroundRepeat: 'no-repeat',
-            position: 'relative',
-          }} className="related-thumbnail">
-        </div>
+  return (
+    <div id='wrapper'>
+      <button
+        className="prev"
+        onClick={() => {
+          scroll(-1);
+        }}>
+        {'<'}
+      </button>
+      <div className="related-image-container" id="related-image-container">
+        {_.map(related.styles, (style, i) => {
+          return (
+            <div
+              key={i}
+              style={{
+                backgroundImage: `url(${style.results[0].photos[0].thumbnail_url})`,
+                backgroundRepeat: 'no-repeat',
+                position: 'relative',
+              }} className="related-image-thumbnail">
+            </div>
+          );
+        })}
       </div>
-    );
-  });
+      <button
+        className="next"
+        onClick={() => {
+          scroll(1);
+        }}>
+        {'>'}
+      </button>
+    </div>
+  );
 };
