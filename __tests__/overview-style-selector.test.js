f6271a97d5585f8a24c28d95165c1cd78412d2dd
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-27 06:40:49 -0700

    Test

--- __tests__/overview-style-selector.test.js
+++ __tests__/overview-style-selector.test.js
@@ -4,16 +4,20 @@ Import any required packages:
 const package = require('package_url');
  */
 
-import React from 'react';
-import selectStyle from '../client/src/state/actions/selectStyle';
-import StyleSelector from '../client/src/components/Overview/StyleSelector.jsx';
-import { create } from 'react-test-renderer';
-import { Provider } from 'react-redux';
-import configureStore from 'redux-mock-store';
-import store from '../client/src/state/store';
+// import React from 'react';
+// import selectStyle from '../client/src/state/actions/selectStyle';
+// import StyleSelector from '../client/src/components/Overview/StyleSelector.jsx';
+// import { create } from 'react-test-renderer';
+// import { Provider } from 'react-redux';
+// import configureStore from 'redux-mock-store';
+// import store from '../client/src/state/store';
 
 
-test('should send proper style as state to redux store', () => {
-  const mockStore = configureStore({});
-  let component = create(<Provider store={store}><StyleSelector /></Provider>);
-})
+// test('should send proper style as state to redux store', () => {
+//   const mockStore = configureStore({});
+//   let component = create(<Provider store={store}><StyleSelector /></Provider>);
+// })
+
+it('Confirms that Jest works', () => {
+  expect(3).toEqual(3);
+});
