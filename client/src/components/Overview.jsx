3668b29c0ec9bf1dcb3cf67e979511241fecf739
Sean Lawlor <lawlorseanr@gmail.com>
2021-07-28 16:18:49 -0700

    Refactor image gallery for expanded view to replace entire top view div

--- client/src/components/Overview.jsx
+++ client/src/components/Overview.jsx
@@ -1,25 +1,23 @@
 import React from 'react';
-
-import ImageGallery from './Overview/ImageGallery.jsx';
-import ProductSummary from './Overview/ProductSummary.jsx';
-import StyleSelector from './Overview/StyleSelector.jsx';
-import AddToCart from './Overview/AddToCart.jsx';
+import { useSelector, useDispatch } from 'react-redux';
 import ProductDescription from './Overview/ProductDescription.jsx';
 import ProductShare from './Overview/ProductShare.jsx';
 
+import TopView from './Overview/TopView.jsx';
+
 export default () => {
+
+  const state = useSelector(state => {
+    return {
+      style: state.product.styleInfo.results[state.style],
+      thumbnail: state.thumbnail,
+      defaultView: state.defaultView,
+    };
+  });
+
   return (
     <div id="body-overview">
-      <div id="body-overview-top">
-        <div id="body-overview-top-left">
-          <ImageGallery />
-        </div>
-        <div id="body-overview-top-right">
-          <ProductSummary />
-          <StyleSelector />
-          <AddToCart />
-        </div>
-      </div>
+      <TopView />
       <div id="body-overview-bottom">
         <div id="body-overview-bottom-left">
           <ProductDescription />
