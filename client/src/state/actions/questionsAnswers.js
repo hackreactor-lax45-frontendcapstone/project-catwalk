2c729a28cb0af48fb5eabb54c8ca54a0a68b6264
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-30 12:31:34 -0700

    Actions, reducers fixed, styles folder added

--- client/src/state/actions/questionsAnswers.js
+++ client/src/state/actions/questionsAnswers.js
@@ -1,17 +1,26 @@
 import axios from 'axios';
 import AtelierAPI from '../../lib/atelierAPI';
 
-export default (dispatch, productID) => {
+export default (dispatch, productID, page, count) => {
   const questionsQuery = axios({
     method: 'get',
-    url: `${AtelierAPI.url}/qa/questions`,
-    headers: AtelierAPI.headers
+    url: `${AtelierAPI.url}/qa/questions/`,
+    headers: AtelierAPI.headers,
+    params: {
+      product_id: productID,
+      page,
+      count
+    }
   });
 
   const answersQuery = axios({
     method: 'get',
     url: `${AtelierAPI.url}/qa/questions/${productID}/answers`,
     headers: AtelierAPI.headers
+    params: {
+      page,
+      count
+    }
   });
 
   Promise.all([
--- client/src/state/reducers/questionsAnswers.js
+++ client/src/state/reducers/questionsAnswers.js
@@ -1,27 +1,54 @@
 import Redux from 'redux';
-
+/*
+{
+  questions,
+  answers
+}
+*/
 const initialState = {
-  "product_id": "5",
-  "results": [{
-        "question_id": undefined,
-        "question_body": '',
-        "question_date": '',
-        "asker_name": '',
-        "question_helpfulness": undefined,
-        "reported": null,
-        "answers": {
-          '': {
-            "id": undefined,
-            "body": '',
-            "date": '',
-            "answerer_name": '',
-            "helpfulness": undefined,
-            "photos": []
+  questions: {
+    "product_id": "5",
+    "results": [{
+          "question_id": undefined,
+          "question_body": '',
+          "question_date": '',
+          "asker_name": '',
+          "question_helpfulness": undefined,
+          "reported": null,
+          "answers": {
+            '': {
+              "id": undefined,
+              "body": '',
+              "date": '',
+              "answerer_name": '',
+              "helpfulness": undefined,
+              "photos": []
+            }
           }
-        }
-  }]
+    }]
+  },
+  answers: {
+    "question": '',
+    "page": undefined,
+    "count": undefined,
+    "results": [
+      {
+        "answer_id": undefined,
+        "body": '',
+        "date": '',
+        "answerer_name": '',
+        "helpfulness": undefined,
+        "photos": [],
+      }
+    ]
+  }
 };
 
-const reducer = () => {
-  switch
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case: 'QUESTION_ANSWER':
+
+    default:
+      return state;
+  }
 }
