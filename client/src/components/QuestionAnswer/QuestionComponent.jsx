71b13d094bdebf477c57e7f27e5ae421cb9ca5d9
Tyler Jones <tylerj.audio94@gmail.com>
2021-07-30 12:28:06 -0700

    Boiler plate code added

--- client/src/components/QuestionAnswer/QuestionComponent.jsx
+++ client/src/components/QuestionAnswer/QuestionComponent.jsx
@@ -2,6 +2,13 @@ import React from 'react';
 
 export default props => {
   return (
-    <div>QuestionComponent</div>
+    <div>
+      <div id="qa-in-left">
+        <div id="qa-question"></div>
+        <div id="qa-answer"></div>
+        <div id="qa-answer-info">by username, date | Helpful? Yes 0 | Report</div>
+      </div>
+      <div id="qa-in-right"></div>
+    </div>
   );
 };
