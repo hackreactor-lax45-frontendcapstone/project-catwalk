479528c6d995268da8c7ce255bfe314351c5f04d
Tyler Jones <tylerj.audio94@gmail.com>
2021-08-02 08:24:12 -0700

    Add a Question layout , need to implement functionality

--- client/src/components/QuestionAnswer/AddQuestion.jsx
+++ client/src/components/QuestionAnswer/AddQuestion.jsx
@@ -2,6 +2,27 @@ import React from 'react';
 
 export default props => {
   return (
-    <div>AddQuestion</div>
+    <div>
+      <button></button>
+      <div id="qa-question-modal" className="question-modal">
+        <div className="question-modal-header">
+          <div className="question-modal-title">Ask Your Question</div>
+          <div className="question-modal-subtitle">PRODUCT_NAME_HERE</div>
+        </div>
+        <div className="question-modal-body">
+          <label>Your Question * </label>
+          <input></input>
+
+          <label>What is your nickname? * </label>
+          <input></input>
+
+          <label>Your email * </label>
+          <input></input>
+
+          <button>Submit Question</button>
+        </div>
+      <button>&times;</button>
+      </div>
+    </div>
   );
 };
