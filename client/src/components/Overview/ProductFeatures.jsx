b3b7df893a71783f15859f1f8a01a353e317dc3a
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-02 10:55:32 -0700

    Implement dynamic feature list based on productInfo features

--- client/src/components/Overview/ProductFeatures.jsx
+++ client/src/components/Overview/ProductFeatures.jsx
@@ -1,23 +1,25 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
 import '../../../dist/styles/overview/ProductFeatures.css';
 
+const FeatureRow = ({ feature }) => {
+  return (
+    <tr className='features-row'>
+      <td className='features-col'>{feature.feature}</td>
+      <td className='features-col'>{feature.value}</td>
+    </tr>
+  );
+};
+
 export default () => {
+  let features = useSelector(state => state.product.productInfo.features);
   return (
     <div id='body-overview-features-container'>
+      <span id='features-label'>Product Features</span>
       <table id='features-table'>
         <tbody id='features-tbody'>
-          <tr className='features-row'>
-            <td className='features-col features-firstcol'>a</td>
-            <td className='features-col features-secondcol'>b</td>
-          </tr>
-          <tr className='features-row'>
-            <td className='features-col features-firstcol'>a</td>
-            <td className='features-col features-secondcol'>b</td>
-          </tr>
-          <tr className='features-row'>
-            <td className='features-col features-firstcol'>a</td>
-            <td className='features-col features-secondcol'>b</td>
-          </tr>
+          {_.map(features, (feature, i) => <FeatureRow key={i} feature={feature}/>)}
         </tbody>
       </table>
     </div>
