9d881b90f874faf0619716147de8db61b68ec2f8
Anna <a.solilee@gmail.com>
2021-08-02 16:11:11 -0700

    Work in progress

--- client/src/components/RatingsReviews/ReviewComponent.jsx
+++ client/src/components/RatingsReviews/ReviewComponent.jsx
@@ -1,7 +1,8 @@
 /* eslint-disable indent */
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import moment from 'moment';
+import actions from '../../state/actions/index.js';
 
 const ReviewComponent = () => {
   const reviews = useSelector((state) => {
@@ -29,7 +30,7 @@ const ReviewComponent = () => {
           <path d="M2.5 8a5.5 5.5 0 0 1 8.25-4.764.5.5 0 0 0 .5-.866A6.5 6.5 0 1 0 14.5 8a.5.5 0 0 0-1 0 5.5 5.5 0 1 1-11 0z"/>
           <path d="M15.354 3.354a.5.5 0 0 0-.708-.708L8 9.293 5.354 6.646a.5.5 0 1 0-.708.708l3 3a.5.5 0 0 0 .708 0l7-7z"/>
          </svg>
-         I recommend this product
+         <span>  I recommend this product</span>
         </div>
       );
     }
