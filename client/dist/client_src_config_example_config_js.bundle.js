0fd2723c451f044c5d4656049da74efa30208af3
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-02 19:28:32 -0700

    Remove coverage from tracking, update AtelierAPI to handle non-existent config.js during CircleCI testing

---
+++ client/dist/client_src_config_example_config_js.bundle.js
@@ -0,0 +1,22 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(self["webpackChunkproject_catwalk"] = self["webpackChunkproject_catwalk"] || []).push([["client_src_config_example_config_js"],{
+
+/***/ "./client/src/config/example.config.js":
+/*!*********************************************!*\
+  !*** ./client/src/config/example.config.js ***!
+  \*********************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (__WEBPACK_DEFAULT_EXPORT__)\n/* harmony export */ });\n/*\nCreate a file named `config.js` in this same folder with your real github token.\n */\nvar TOKEN = 'insert token';\n/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (TOKEN);\n\n//# sourceURL=webpack://project-catwalk/./client/src/config/example.config.js?");
+
+/***/ })
+
+}]);
