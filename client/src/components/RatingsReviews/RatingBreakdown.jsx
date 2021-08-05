e7edb5f500252970321dc63f1dfd2b8fa16b1c08
Anna <a.solilee@gmail.com>
2021-08-04 22:46:05 -0700

    Added visual for average review ratings to ratings breakdown

--- client/src/components/RatingsReviews/RatingBreakdown.jsx
+++ client/src/components/RatingsReviews/RatingBreakdown.jsx
@@ -1,6 +1,7 @@
 import React, {useState} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import actions from '../../state/actions';
+import '../../../dist/styles/ratingsreviews/ratingBreakdown.css';
 
 const ratingsBreakdown = () => {
   const dispatch = useDispatch();
@@ -11,7 +12,6 @@ const ratingsBreakdown = () => {
     };
   });
 
-
   const getAverageRating = () => {
     let ratings = 0;
     let count = 0;
@@ -21,6 +21,22 @@ const ratingsBreakdown = () => {
     }
     let averageRating = Math.round((ratings / count * 10)) / 10;
     return averageRating;
+  };
+
+  const getWidth = () => {
+    let ratings = 0;
+    let count = 0;
+    for (var key in state.metadata.ratings) {
+      count += Number(state.metadata.ratings[key]);
+      ratings += (Number(key) * Number(state.metadata.ratings[key]));
+    }
+    let starPercentage = (ratings / count) / 5 * 100;
+    if ((starPercentage % 5) < 2.5) {
+      starPercentage -= (starPercentage % 5);
+    } else {
+      starPercentage += (5 - (starPercentage % 5));
+    }
+    return starPercentage;
   };
 
   const filterMessage = () => {
@@ -38,6 +54,12 @@ const ratingsBreakdown = () => {
     <div className="rating-breakdown">
       RatingBreakdown
       <div>{getAverageRating()}</div>
+
+      <span id="average-rating-star">
+        <span className="average-rating-star-outer">
+          <span className="average-rating-star-inner" style={{ width: `${getWidth()}%`}}></span>
+        </span>
+      </span>
 
       <div onClick={() => { (dispatch(actions.selectFilters(5))); }}>5 stars</div>
       <div onClick={() => { (dispatch(actions.selectFilters(4))); }}>4 stars</div>
