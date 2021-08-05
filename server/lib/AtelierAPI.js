ad2323fa688732e937c462527ca42b94e5fb0fe9
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-05 10:46:28 -0700

    Ignore server.test.js in default jest-test command due to untracked config.js file in git repo

--- server/lib/AtelierAPI.js
+++ server/lib/AtelierAPI.js
@@ -1,4 +1,15 @@
-const TOKEN = require('../config/config.js');
+const fs = require('fs');
+const path = require('path');
+const config = '../config/config.js';
+
+let TOKEN;
+if (fs.existsSync(path.join(__dirname, config))) {
+  TOKEN = require(config)
+} else {
+  TOKEN = require('../config/example.config.js')
+}
+
+// const TOKEN = require('../config/config.js');
 const SERVER = 'https://app-hrsei-api.herokuapp.com/api/fec2';
 const CAMPUS = 'hr-lax';
 const BASE_URL = `${SERVER}/${CAMPUS}`;
