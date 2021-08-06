b305d6de4264f8f05c7f847a8574a94cd21f8682
Ubuntu <ubuntu@ip-172-31-32-19.us-east-2.compute.internal>
2021-08-06 04:30:51 +0000

    Server-side changes

--- client/src/state/actions/selectProduct.js
+++ client/src/state/actions/selectProduct.js
@@ -3,16 +3,20 @@ import { url, config } from '../../lib/Server';
 
 export default (dispatch, productID) => {
 
+  console.log(`${config.baseURL}/${url.products}/${productID}`);
   let productResponse = axios.get(
     `${url.products}/${productID}`,
     config
-  );
+  )
+  .then(response => console.log(response))
+  .catch(err => console.error('Yahtzee!', err));
+  
 
   let stylesResponse = axios.get(
     `${url.products}/${productID}/styles`,
     config
   );
-
+  console.log('before promise');
   return Promise.all([
     productResponse,
     stylesResponse,
