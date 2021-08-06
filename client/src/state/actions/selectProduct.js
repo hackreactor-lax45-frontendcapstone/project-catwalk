8719dd83b435d3716d1d40324577b1a06d10df5d
Sean Lawlor <lawlorseanr@gmail.com>
2021-08-05 21:36:20 -0700

    Reset changes to selectProduct

--- client/src/state/actions/selectProduct.js
+++ client/src/state/actions/selectProduct.js
@@ -3,25 +3,21 @@ import { url, config } from '../../lib/Server';
 
 export default (dispatch, productID) => {
 
-  console.log(`${config.baseURL}/${url.products}/${productID}`);
   let productResponse = axios.get(
     `${url.products}/${productID}`,
     config
-  )
-  .then(response => console.log(response))
-  .catch(err => console.error('Yahtzee!', err));
-  
+  );
 
   let stylesResponse = axios.get(
     `${url.products}/${productID}/styles`,
     config
   );
-  console.log('before promise');
   return Promise.all([
     productResponse,
     stylesResponse,
   ])
     .then(info => {
+      console.log(info);
       let [productInfo, styleInfo] = info;
       dispatch({
         type: 'SELECT_PRODUCT',
